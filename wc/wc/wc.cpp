c30e3580c726a34a613a2e7d0efd093e42820e03
monsterkodi <monsterkodi@gmx.net>
2019-07-17 20:26:40 +0200

    wc

--- wc/wc/wc.cpp
+++ wc/wc/wc.cpp
@@ -40,6 +40,24 @@ bool save_png_memory(HBITMAP hbitmap, std::vector<BYTE>& data)
 
 int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_opt LPSTR lpCmdLine, __in int nShowCmd )
 {
+    /*
+    if (argc < 2)
+    {
+        usage();
+        return 0;
+    }
+    
+    if (cmp(argv[1], L"help"))
+    {
+        if (argc == 2) usage();
+        else           help(argv[2]);
+    }
+    else if (cmp(argv[1], L"folder"))
+    {
+        if (argc == 2) help(argv[1]);
+        else           folder(argv[2]);
+    }*/
+    
     CoInitialize(NULL);
 
     ULONG_PTR token;
@@ -73,6 +91,133 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
     CoUninitialize();
 
 	return 0;
+}
+
+
+#include <windows.h>
+#include <initguid.h>
+#include <KnownFolders.h>
+#include <ShlObj.h>
+#include <wchar.h>
+// #include <shobjidl.h>
+#include <shtypes.h>
+
+#define true  1
+#define false 0
+
+int cmp(wchar_t *a, wchar_t *b)
+{
+    return lstrcmpiW(a, b) == 0;
+} 
+
+int log(wchar_t *msg)
+{
+	wprintf(msg);
+	wprintf(L"\n");
+    return 0;
+}
+
+int error(wchar_t *msg)
+{
+    log(msg);
+    return 1;
+}
+
+// 000   000   0000000   0000000    0000000   00000000  
+// 000   000  000       000   000  000        000       
+// 000   000  0000000   000000000  000  0000  0000000   
+// 000   000       000  000   000  000   000  000       
+//  0000000   0000000   000   000   0000000   00000000  
+
+int usage(void)
+{
+    log(L"");
+    log(L"wxw [command] [options...]\n");
+    log(L"     help     <command>");
+    log(L"     folder   <name>");
+    log(L"");
+    return 0;
+}
+
+int help(wchar_t *command)
+{
+	if (cmp(command, L"folder"))
+	{
+        log(L"");
+        log(L"folder names:");
+        log(L"");
+        log(L"       AppData");
+        log(L"       Desktop");
+        log(L"       Documents");
+        log(L"       Downloads");
+        log(L"       Fonts");
+        log(L"       Program");
+        log(L"       ProgramX86");
+        log(L"       Startup");
+        log(L"");
+	}
+    return 0;
+}
+
+// 00000000   0000000   000      0000000    00000000  00000000   
+// 000       000   000  000      000   000  000       000   000  
+// 000000    000   000  000      000   000  0000000   0000000    
+// 000       000   000  000      000   000  000       000   000  
+// 000        0000000   0000000  0000000    00000000  000   000  
+
+int folder(wchar_t *id)
+{
+    int result = 0;
+    PWSTR path = NULL;
+    HRESULT hr = S_OK; 
+    
+    // doesn't work :( FOLDERID_RecycleBinFolder 
+    
+    if      (cmp(id, L"AppData"))    { hr = SHGetKnownFolderPath(&FOLDERID_RoamingAppData,  0, NULL, &path); }
+    else if (cmp(id, L"Desktop"))    { hr = SHGetKnownFolderPath(&FOLDERID_Desktop,         0, NULL, &path); }
+    else if (cmp(id, L"Documents"))  { hr = SHGetKnownFolderPath(&FOLDERID_Documents,       0, NULL, &path); }
+    else if (cmp(id, L"Downloads"))  { hr = SHGetKnownFolderPath(&FOLDERID_Downloads,       0, NULL, &path); }
+    else if (cmp(id, L"Fonts"))      { hr = SHGetKnownFolderPath(&FOLDERID_Fonts,           0, NULL, &path); }
+    else if (cmp(id, L"Program"))    { hr = SHGetKnownFolderPath(&FOLDERID_ProgramFiles,    0, NULL, &path); }
+    else if (cmp(id, L"ProgramX86")) { hr = SHGetKnownFolderPath(&FOLDERID_ProgramFilesX86, 0, NULL, &path); }
+    else if (cmp(id, L"Startup"))    { hr = SHGetKnownFolderPath(&FOLDERID_Startup,         0, NULL, &path); }
+    else if (cmp(id, L"Recycle")) 
+    {
+        LPSHELLFOLDER pDesktop       = NULL;
+        LPITEMIDLIST  pidlRecycleBin = NULL;
+
+        hr = SHGetDesktopFolder(&pDesktop);
+        wprintf(L"SHGetDesktopFolder %d\n", hr);
+        
+        hr = SHGetFolderLocation(NULL, CSIDL_BITBUCKET, NULL, 0, &pidlRecycleBin);
+        wprintf(L"SHGetFolderLocation %d\n", hr);
+        
+        // hr = pDesktop->BindToObject(pidlRecycleBin, NULL, IID_IShellFolder, (LPVOID *)&m_pRecycleBin);
+        // wprintf(L"BindToObject %d\n", hr);    
+        
+        // STRRET strRet;
+        // hr = pDesktop->GetDisplayNameOf(pidlRecycleBin, SHGDN_NORMAL, &strRet);
+        // wprintf(L"GetDisplayNameOf %d\n", hr);
+    }
+    else
+    {
+        wprintf(L"ERROR: invalid folder name '%ls'\n", id);
+        result = 1;
+    }
+
+    if (SUCCEEDED(hr) && path) 
+    {
+        wprintf(L"folder %ls\n", path);
+    }
+    else
+    {
+        wprintf(L"ERROR: can't get folder '%ls' %ls\n", id, path);
+        result = 1;
+    }
+
+    CoTaskMemFree(path);
+    
+    return result;
 }
 
 
