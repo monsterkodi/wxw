441af01e2990c09cc5bd9182ff945c37f35c05ad
monsterkodi <monsterkodi@gmx.net>
2019-07-17 20:22:32 +0200

    screenshot

--- wc/wc/wc.cpp
+++ wc/wc/wc.cpp
@@ -3,221 +3,76 @@
 #include <tchar.h>
 #include <math.h>
 #include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include <gdiplus.h>
 
 using namespace Gdiplus;
+using namespace std;
 
-HBITMAP CopyDCToBitmap(HDC hScrDC, LPRECT lpRect);
-BOOL SaveBmp(HBITMAP hBitmap, LPCWSTR FileName);
-
-void GetCLSID(const WCHAR* format, CLSID* pClsid) 
+bool save_png_memory(HBITMAP hbitmap, std::vector<BYTE>& data)
 {
-	UINT  num = 0;  // number of image encoders
-	UINT  size = 0; // size of the image encoder array in bytes
+    Gdiplus::Bitmap bmp(hbitmap, nullptr);
 
-	ImageCodecInfo* pImageCodecInfo = NULL;
+    IStream* istream = nullptr;
+    CreateStreamOnHGlobal(NULL, TRUE, &istream);
 
-	GetImageEncodersSize(&num, &size);
+    CLSID clsid_png;
+    CLSIDFromString(L"{557cf406-1a04-11d3-9a73-0000f81ef32e}", &clsid_png);
+    Gdiplus::Status status = bmp.Save(istream, &clsid_png);
+    if (status != Gdiplus::Status::Ok)
+        return false;
 
-	pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
+    HGLOBAL hg = NULL;
+    GetHGlobalFromStream(istream, &hg);
 
-	GetImageEncoders(num, size, pImageCodecInfo);
+    SIZE_T bufsize = GlobalSize(hg);
+    data.resize(bufsize);
 
-	for (UINT j = 0; j < num; ++j)
-	{
-		if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0)
-		{
-			*pClsid = pImageCodecInfo[j].Clsid;
-			free(pImageCodecInfo);
-		}
-	}
-}
+    LPVOID pimage = GlobalLock(hg);
+    memcpy(&data[0], pimage, bufsize);
+    GlobalUnlock(hg);
 
-Gdiplus::Status HBitmapToBitmap(HBITMAP source, Gdiplus::PixelFormat pixel_format, Gdiplus::Bitmap** result_out)
-{
-	BITMAP source_info = { 0 };
-	if (!::GetObject(source, sizeof(source_info), &source_info))
-	{ 
-		return Gdiplus::GenericError;
-	}
-
-	Gdiplus::Status s;
-
-	Gdiplus::Bitmap* target = new Gdiplus::Bitmap(source_info.bmWidth, source_info.bmHeight, pixel_format);
-
-	if (!target)
-	{ 
-		return Gdiplus::OutOfMemory;
-	}
-	if ((s = target->GetLastStatus()) != Gdiplus::Ok)
-	{ 
-		return s;
-	}
-
-	Gdiplus::BitmapData target_info;
-	Gdiplus::Rect rect(0, 0, source_info.bmWidth, source_info.bmHeight);
-
-	s = target->LockBits(&rect, Gdiplus::ImageLockModeWrite, pixel_format, &target_info);
-	if (s != Gdiplus::Ok) return s;
-
-	if (target_info.Stride != source_info.bmWidthBytes)
-	{ 
-		return Gdiplus::InvalidParameter; // pixel_format is wrong!
-	}
-
-	if (!source_info.bmBits)
-	{
-		return Gdiplus::InvalidParameter; // no pixels?
-	}
-
-	CopyMemory(target_info.Scan0, source_info.bmBits, source_info.bmWidthBytes * source_info.bmHeight);
-
-	s = target->UnlockBits(&target_info);
-	if (s != Gdiplus::Ok) return s;
-
-	*result_out = target;
-
-	return Gdiplus::Ok;
+    istream->Release();
+    return true;
 }
 
 int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_opt LPSTR lpCmdLine, __in int nShowCmd )
 {
-	GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR gdiplusToken;
-	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+    CoInitialize(NULL);
 
-	RECT rect;
-	rect.left   = 0;
-	rect.top    = 0;
-	rect.right  = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-	rect.bottom = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    ULONG_PTR token;
+    Gdiplus::GdiplusStartupInput tmp;
+    Gdiplus::GdiplusStartup(&token, &tmp, NULL);
 
-	HBITMAP bitmap = CopyDCToBitmap(GetDC(GetDesktopWindow()), &rect);
-	
-// 	Gdiplus::Bitmap image(bitmap, NULL);
+    RECT rc;
+    GetClientRect(GetDesktopWindow(), &rc);
+    cout << "rect" << rc.right << rc.bottom << endl;
+    rc.right  = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    rc.bottom = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    
+    auto hdc = GetDC(0);
+    auto memdc = CreateCompatibleDC(hdc);
+    auto hbitmap = CreateCompatibleBitmap(hdc, rc.right, rc.bottom);
+    auto oldbmp = SelectObject(memdc, hbitmap);
+    BitBlt(memdc, 0, 0, rc.right, rc.bottom, hdc, 0, 0, SRCCOPY);
+    SelectObject(memdc, oldbmp);
+    DeleteDC(memdc);
+    ReleaseDC(0, hdc);
 
-	CLSID clsID;
-	GetCLSID(L"image/png", &clsID);
-	
-	Bitmap* image = NULL;
-	HBitmapToBitmap(bitmap, PixelFormat32bppRGB, &image);
+    std::vector<BYTE> data;
+    if (save_png_memory(hbitmap, data))
+    {
+        std::ofstream fout("screenshot.png", std::ios::binary);
+        fout.write((char*)data.data(), data.size());
+    }
+    DeleteObject(hbitmap);
 
-// 	Image* image = new Image(bitmap, NULL);
-	if (image)
-	{ 
-		Status result = image->Save(L"screenshot2.png", &clsID, NULL);
-	}
-
-	SaveBmp(bitmap, L"screenshot.bmp");
-
-// 	Image* image = new Image();
-// 	GetEncoderClsid(L"image/png", &encoderClsid);
-// 	stat = image->Save(L"Bird.png", &encoderClsid, NULL);
-	//bitmap->
-	//.Save("icon.png", ImageFormat.Png);
-
-	delete image;
-
-	GdiplusShutdown(gdiplusToken);
+    Gdiplus::GdiplusShutdown(token);
+    CoUninitialize();
 
 	return 0;
 }
 
-HBITMAP CopyDCToBitmap(HDC hScrDC, LPRECT lpRect)
-{
-	HDC hMemDC; 
-	HBITMAP hBitmap,hOldBitmap; 
-	int nX, nY, nX2, nY2; 
-	int nWidth, nHeight; 
-
-	if (IsRectEmpty(lpRect)) return NULL;
-
-	nX  = lpRect->left;
-	nY  = lpRect->top;
-	nX2 = lpRect->right;
-	nY2 = lpRect->bottom;
-	nWidth = nX2 - nX;
-	nHeight = nY2 - nY;
-	hMemDC = CreateCompatibleDC(hScrDC);
-	hBitmap = CreateCompatibleBitmap(hScrDC, nWidth, nHeight);
-	hOldBitmap = (HBITMAP)SelectObject(hMemDC, hBitmap);
-	StretchBlt(hMemDC,0,0,nWidth,nHeight,hScrDC,nX,nY,nWidth,nHeight,SRCCOPY);
-	//BitBlt(hMemDC, 0, 0, nWidth, nHeight,hScrDC, nX, nY, SRCCOPY);
-	hBitmap = (HBITMAP)SelectObject(hMemDC, hOldBitmap);
-
-	DeleteDC(hMemDC);
-	DeleteObject(hOldBitmap);
-	return hBitmap;
-}
-
-BOOL SaveBmp(HBITMAP hBitmap, LPCWSTR FileName)
-{
-	HDC hDC;
-	int iBits;
-	WORD wBitCount;
-	DWORD dwPaletteSize=0, dwBmBitsSize=0, dwDIBSize=0, dwWritten=0; 
-	BITMAP Bitmap; 
-	BITMAPFILEHEADER bmfHdr; 
-	BITMAPINFOHEADER bi; 
-	LPBITMAPINFOHEADER lpbi; 
-	HANDLE fh, hDib, hPal,hOldPal=NULL; 
-
-	hDC = CreateDC( _T( "DISPLAY" ), NULL, NULL, NULL);
-	iBits = GetDeviceCaps(hDC, BITSPIXEL) * GetDeviceCaps(hDC, PLANES); 
-	DeleteDC(hDC); 
-	if (iBits <= 1) wBitCount = 1; 
-	else if (iBits <= 4) wBitCount = 4; 
-	else if (iBits <= 8) wBitCount = 8; 
-	else wBitCount = 24; 
-
-	GetObject(hBitmap, sizeof(Bitmap), (LPSTR)&Bitmap);
-	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = Bitmap.bmWidth;
-	bi.biHeight = Bitmap.bmHeight;
-	bi.biPlanes = 1;
-	bi.biBitCount = wBitCount;
-	bi.biCompression = BI_RGB;
-	bi.biSizeImage = 0;
-	bi.biXPelsPerMeter = 0;
-	bi.biYPelsPerMeter = 0;
-	bi.biClrImportant = 0;
-	bi.biClrUsed = 0;
-
-	dwBmBitsSize = ((Bitmap.bmWidth * wBitCount + 31) / 32) * 4 * Bitmap.bmHeight;
-
-	hDib = GlobalAlloc(GHND,dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER)); 
-	lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib); 
-	*lpbi = bi; 
-	hPal = GetStockObject(DEFAULT_PALETTE); 
-	if (hPal) 
-	{ 
-		hDC = ::GetDC(NULL); 
-		hOldPal = ::SelectPalette(hDC, (HPALETTE)hPal, FALSE); 
-		RealizePalette(hDC); 
-	}
-	GetDIBits(hDC, hBitmap, 0, (UINT) Bitmap.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER) + dwPaletteSize, (BITMAPINFO *)lpbi, DIB_RGB_COLORS); 
-
-	if (hOldPal) 
-	{ 
-		::SelectPalette(hDC, (HPALETTE)hOldPal, TRUE); 
-		RealizePalette(hDC); 
-		::ReleaseDC(NULL, hDC); 
-	} 
-	fh = CreateFile( FileName, GENERIC_WRITE,0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL ); 
-
-	if (fh == INVALID_HANDLE_VALUE) return FALSE; 
-
-	bmfHdr.bfType = 0x4D42; // "BM" 
-	dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize; 
-	bmfHdr.bfSize = dwDIBSize; 
-	bmfHdr.bfReserved1 = 0; 
-	bmfHdr.bfReserved2 = 0; 
-	bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize; 
-	WriteFile(fh, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER), &dwWritten, NULL); 
-	WriteFile(fh, (LPSTR)lpbi, dwDIBSize, &dwWritten, NULL); 
-	GlobalUnlock(hDib); 
-	GlobalFree(hDib); 
-	CloseHandle(fh); 
-	return TRUE;
-}
 
