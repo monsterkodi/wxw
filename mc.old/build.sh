c7345d7accaaffdb5860f9b25627d3383c40b676
monsterkodi <monsterkodi@gmx.net>
2019-08-10 01:52:09 +0200

    ignore

--- mc.old/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`
-
-xcodebuild -workspace mc.xcworkspace -scheme mc -configuration Release
--- mc/Pods/Target Support Files/Pods-mc/Pods-mc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mc : NSObject
-@end
-@implementation PodsDummy_Pods_mc
-@end
